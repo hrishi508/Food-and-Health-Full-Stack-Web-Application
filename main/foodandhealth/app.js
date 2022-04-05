968745f9953ecfb742d747b1ba036d6e48169a78
hitarthk9 <hitarthkothari9@gmail.com>
2022-04-05 23:29:06 +0530

    Add get request and css support for nodemon

--- main/foodandhealth/app.js
+++ main/foodandhealth/app.js
@@ -1,41 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+const https = require('https');
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+// return the index.html file when a GET request is made to the root path "/"
+app.get("/", function (req, res) {
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get("/cuisines", function (req, res) {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.sendFile(__dirname + "/views/html5up-phantom/landing.html");
 });
 
-module.exports = app;
+var cuisine;
+app.get("/food_item", function (req, res) {
+  cuisine = req.query.cuisine;
+  console.log(cuisine);
+  res.sendFile(__dirname + "/views/html5up-phantom/landing.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
+});
+
+// app.get("/diet", function (req, res) {
+
+//   res.sendFile(__dirname + "views/diet.html");
+// });
+
+// app.post("/diet", function(req, res) {
+//   var c = req.body.city;
+
+// });
+
