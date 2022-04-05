fe1dbcc100a379f4f10d790e1cc7aa88a4bbaacf
hrishi508 <k.hrishi2010@gmail.com>
2022-04-05 16:02:35 +0530

    Delete sample

--- Top 50 Most Appetizing Designs for Food Websites_files/439788(2).js
+++
@@ -1,6 +0,0 @@
-// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
-
-var _hsp = window._hsp = window._hsp || [];
- _hsp.push(['addEnabledFeatureGates', []]);
-!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hs-banner.com/439788.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-439788",0,{"data-cookieconsent":"ignore","data-hs-ignore":true,"data-loader":"hs-scriptloader","data-hsjs-portal":439788,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
-!function(e,t){if(!document.getElementById(e)){var c=document.createElement("script");c.src="https://js.hs-analytics.net/analytics/1648919100000/439788.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
